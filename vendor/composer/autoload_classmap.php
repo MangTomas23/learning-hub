bd17243ac514389f391cd7114f4cfe183e78d15b
MangTomas23 <matos.adrianpaul@gmail.com>
2015-10-16 18:03:17 +0800

    Created Subject model and migration

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -9,6 +9,7 @@ return array(
     'AddFirstnameLastnameToUsersTable' => $baseDir . '/database/migrations/2015_10_15_171539_add_firstname_lastname_to_users_table.php',
     'AddTypeToUsersTable' => $baseDir . '/database/migrations/2015_10_15_170713_add_type_to_users_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateSubjectsTable' => $baseDir . '/database/migrations/2015_10_16_100238_create_subjects_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
