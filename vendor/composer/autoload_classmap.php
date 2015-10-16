d2b86a7a962e5a6012ffe5f08380142d21542ecd
MangTomas23 <matos.adrianpaul@gmail.com>
2015-10-17 00:02:13 +0800

    Added USN to table users

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'AddFirstnameLastnameToUsersTable' => $baseDir . '/database/migrations/2015_10_15_171539_add_firstname_lastname_to_users_table.php',
     'AddTypeToUsersTable' => $baseDir . '/database/migrations/2015_10_15_170713_add_type_to_users_table.php',
+    'AddUsnToUsers' => $baseDir . '/database/migrations/2015_10_16_160014_add_usn_to_users.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
     'CreateSubjectsTable' => $baseDir . '/database/migrations/2015_10_16_100238_create_subjects_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
