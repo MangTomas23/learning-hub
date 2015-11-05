c6da2cf273c49ad4c6e620d189c988635f150e77
MangTomas23 <matos.adrianpaul@gmail.com>
2015-11-05 14:37:14 +0800

    Added Quiz model

---
+++ database/migrations/2015_11_05_063004_create_quizzes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->string('title');
+            $table->integer('notes');
+            $table->integer('duration');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('quizzes');
+    }
+}
