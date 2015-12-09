793e59f59968ae695814e4e071e3cae78b4f0eca
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-09 20:00:12 +0800

    Add attempts to quiz column

---
+++ database/migrations/2015_12_09_112618_add_attempts.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAttempts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->integer('attempts')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->dropColumn('attempts');
+        });
+    }
+}
