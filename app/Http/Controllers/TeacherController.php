8d0fde871a07f1141b0a60ad276c3af3f3fefdef
MangTomas23 <matos.adrianpaul@gmail.com>
2015-12-17 01:19:07 +0800

    Check attempt

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -175,6 +175,7 @@ class TeacherController extends Controller
         $quiz = Quiz::find($id);
         $quiz->no_of_items = $quiz->questions->count();
         $quiz['results'] = DB::table('results')->select(DB::raw('id, max(score) as score, quiz_id, user_id'))
+                            ->where('quiz_id', $id)
                             ->groupBy('user_id')->orderBy('score')->get();
         foreach ($quiz['results'] as $result) {
 
